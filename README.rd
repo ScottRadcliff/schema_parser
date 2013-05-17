adfd18dffd33d9a56bb47f4aba55be19be13ef8b
Scott Radcliff <radcliffsc@gmail.com>
2013-05-17 15:24:31 -0400

    Adding ignored attributes for the schema file

---
+++ README.rd
@@ -0,0 +1,8 @@
+# Schema Parser
+
+
+
+## Features
+
+* Parse schema files for model and it's attributes
+* Create a config file for ignored attributes (field names)
---
+++ lib/parser.rb
@@ -0,0 +1,58 @@
+module SchemaParser
+  class Parser
+
+    def initialize(file)
+      text = file.read
+      @data = []
+      Parser.find_models(text).each do |model|
+        model_attributes = Parser.find_attributes(text, model)
+        @data << {:model => model, :attributes => model_attributes}
+      end
+    end
+
+    def data
+      @data
+    end
+
+    def self.find_models(string)
+      models = []
+      string.lines.each do |line|
+        if line.include? "create_table"
+          models << line.scan(/\w+/)[1]
+        end
+      end
+      models
+    end
+
+
+    def self.find_attributes(source, model)
+      @counter = 0
+      @start, @stop = nil, nil
+      source.lines.each do |line|
+       set_start_and_stop_point(line, model)
+      end
+      perform_attribute_extraction(source.lines.slice!(@start..@stop))
+    end
+
+    private
+    def self.perform_attribute_extraction(collection)
+      attributes = []
+      collection.slice!(1, collection.size - 1).each do |line|
+        scanned_word = line.scan(/\w+/)[2]
+        attributes << scanned_word unless scanned_word.nil? || SchemaParser.ignored_attribute?
+      end
+      attributes
+    end
+
+    def self.set_start_and_stop_point(line, model)
+      if line.include? model
+        @start = @counter
+      elsif !@start.nil? && @stop.nil?
+        if line.include? "end"
+          @stop = @counter
+        end
+      end
+       @counter = @counter + 1
+    end
+  end
+end
